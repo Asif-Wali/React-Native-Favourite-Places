008c05e7a73ce23f62014949404e40520d97c722
aasifwali16@gmail.com <aasifwali16@gmail.com>
2024-11-14 02:20:02 +0400

    slight change

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+    <StatusBar style="light" />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
